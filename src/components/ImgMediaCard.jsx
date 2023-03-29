5d6103f56f8ae080281cb0ccb6af03cbf4965b31
icatherena <beresovskyc@gmail.com>
2023-03-29 09:17:31 -0300

    comentarios a evaluarEvoluciones + mapeado a listaEvoluciones + Link to

--- src/components/ImgMediaCard.jsx
+++ src/components/ImgMediaCard.jsx
@@ -11,70 +11,62 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 const ImgMediaCard = (props) => {
   
   const [pokemon, setPokemon] = useState()
+  const [listaEvoluciones, setListaEvoluciones] = useState([])
 
   useEffect(() => {
     setPokemon(props.pokemon);
   },
   [props.pokemon]) 
 
-  const pasarAMayus = (props) => {
-    return (
-      props.name.charAt(0).toUpperCase() + props.name.slice(1)
-    )
-  }
-
-  // const [listaEvoluciones, setListaEvoluciones] = useState([])
-  // const listaMid = []
-
-  // const evaluarEvoluciones = (props) => {
-  //   listaEvoluciones.push(props.initialForm)
-  //   if (props.midForm) {
-  //     if (props.midForm.length > 1) {
-  //       listaMid.push(props.midForm);
-  //       setListaEvoluciones([...listaEvoluciones, listaMid])
-  //     } else {
-  //       listaEvoluciones.push(props.midForm)
-  //     }
-  //   }
-  //   if (props.finalForm && props.finalForm !== undefined) { 
-  //     listaEvoluciones.push(props.finalForm)
-  //   }
-
-  // }
-  
-  /* console.log(listaEvoluciones); */
-
-  const [listaEvoluciones, setListaEvoluciones] = useState([])
- 
   useEffect(() => {
     evaluarEvoluciones(props)
   }, [props])
 
   const evaluarEvoluciones = (props) => {
+    // Se añade el primer pokemon dado que siempre hay una especie inicial, 
+    // la cual puede o no tener una siguiente evolución
     setListaEvoluciones([props.initialForm])
+    // A continuación se evalúa si existe y si contiene un elemento
     if (props.midForm && props.midForm.length > 0) {
+      // Para concatenar los elementos que traería midForm si fuera un array
+      // y no un único elemento,
+      // se crea un segundo array
       let midList = []
+      // Se evalúa si midForm es o no un array de elementos 
       if (Array.isArray(props.midForm)) {
+        // Si cumple la condición, se mapea midForm y se asigna al array midList
         midList = props.midForm.map((item) => item)
+        // A continuación, para añadir los elementos del array midList,
+        // se usa concat para traer una copia de los elementos que ya están
+        // seteados en listaEvoluciones y se añaden los que trae midList
         setListaEvoluciones(prevList => prevList.concat(midList))
       } else {
+        // Si midForm no cumple con la condición (no es un array),
+        // se setea midForm de la misma forma que se hizo con midList usando concat
         setListaEvoluciones(prevList => prevList.concat(props.midForm))
       }
     }
-    if (props.finalForm && props.midForm.length > 0) {
-      /* let finalList = [] */
+    // Como no se tiene certeza de que el pokemon inicial tenga o no
+    // una segunda evolución, se evalua nuevamente si contiene al menos un elemento
+    if (props.finalForm && props.finalForm.length > 0) {
+      // Se evalúa si finalForm tiene algún valor distinto de indefinido o nulo
+      // Si no se evaluara en este momento, se añadiría más adelante
+      // un array de elementos nulos/indefinidos que se mostrarían al mapear
+      // listaEvoluciones dentro del return
       if (props.finalForm !== null && props.finalForm !== undefined) {
+        // Si cumple condición, se evalúa a continuación si se trata de un array
         if (Array.isArray(props.finalForm)) {
-        /* finalList = props.finalForm.map((item) => item)
-        setListaEvoluciones(prevList => prevList.concat(finalList))
-      } else {
-        setListaEvoluciones(prevList => prevList.concat(props.finalForm)) */
+          // Debido a que finalForm a diferencia de initial y midForm trae los elementos como
+          // un array, para concatenarlos y evitar que se añadan a listaEvoluciones como otro array
+          // se recorre el array mediante el uso de for, y sólo si el elemento no es nulo, se añade
+          // a listaEvoluciones
           for (let i = 0; i < props.finalForm.length; i++) {
             if (props.finalForm[i] !== null) {
               setListaEvoluciones(prevList => prevList.concat(props.finalForm[i]))
             }
           }
         } else {
+          // Si no cumple la condición (no es un array, simplemente se añade el elemento a listaEvoluciones)
           setListaEvoluciones(prevList => prevList.concat(props.finalForm))
           }
       }
@@ -84,6 +76,12 @@ const ImgMediaCard = (props) => {
   useEffect(() => {
     console.log("listaEvoluciones actualizada:", listaEvoluciones)
   }, [listaEvoluciones])
+
+  const pasarAMayus = (props) => {
+    return (
+      props.name.charAt(0).toUpperCase() + props.name.slice(1)
+    )
+  }
 
   const theme = createTheme({
     palette: {
@@ -161,10 +159,8 @@ const ImgMediaCard = (props) => {
 
               <Divider variant="body1"><b>CADENA EVOLUTIVA</b></Divider>
               <Typography variant="body1">
-                
-                {listaEvoluciones.join(', ')}
                 {/* {props.initialForm} → {props.midForm} → {props.finalForm} */}
-                {/* { 
+                
                 <ul
                   style = {{
                     listStyleType: 'none', 
@@ -173,7 +169,6 @@ const ImgMediaCard = (props) => {
                     justifyContent: 'space-evenly',
                   }}
                 >
-                  {evaluarEvoluciones(props)}
                   {listaEvoluciones.map((item) => 
                   <li key = {item}>
                     <Link to={`/description/${item}/`} 
@@ -205,7 +200,7 @@ const ImgMediaCard = (props) => {
                             color: '#a6aea6',
                             weight: 'bold'
                           },
-                          '&:visited': 
+                          '&:visited': {
                             color: '#747974',
                             weight: 'bold'
                           }
@@ -213,9 +208,8 @@ const ImgMediaCard = (props) => {
                       />                       
                     </Link>
                    
-                  </li> )}
-                </ul>  
-                } */}
+                  </li> )} 
+                </ul>
                 
               </Typography><br/> 
 
